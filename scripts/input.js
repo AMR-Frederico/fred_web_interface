b86bc5231a88a0047a620f357c4ed42b0e14b74b
DinossauroBebado <guipsilva32@gmail.com>
2023-04-18 16:49:33 -0300

    add pid publisher

---
+++ scripts/input.js
@@ -0,0 +1,119 @@
+const botaoPrintar_l_p = document.getElementById("botao-linear_p");
+botaoPrintar_l_p.addEventListener("click", function() {
+  const inputValor = document.getElementById("input-valor_lp");
+  const valor = inputValor.value;
+  console.log(valor);
+
+  
+  var message = new ROSLIB.Message({
+    data: parseFloat(valor)
+  })
+
+  var topic = new ROSLIB.Topic({
+    ros: ros,
+    name: "/control/position/setup/linear/kp",
+    messageType: "std_msgs/Float32"
+  })
+
+  topic.publish(message);
+});
+
+const botaoPrintar_l_i = document.getElementById("botao-linear_i");
+botaoPrintar_l_i.addEventListener("click", function() {
+  const inputValor = document.getElementById("input-valor_li");
+  const valor = inputValor.value;
+  console.log(valor);
+
+  
+  var message = new ROSLIB.Message({
+    data: parseFloat(valor)
+  })
+
+  var topic = new ROSLIB.Topic({
+    ros: ros,
+    name: "/control/position/setup/linear/ki",
+    messageType: "std_msgs/Float32"
+  })
+
+  topic.publish(message);
+});
+
+const botaoPrintar_l_d = document.getElementById("botao-linear_d");
+botaoPrintar_l_d.addEventListener("click", function() {
+  const inputValor = document.getElementById("input-valor_ld");
+  const valor = inputValor.value;
+  console.log(valor);
+
+  
+  var message = new ROSLIB.Message({
+    data: parseFloat(valor)
+  })
+
+  var topic = new ROSLIB.Topic({
+    ros: ros,
+    name: "/control/position/setup/linear/kd",
+    messageType: "std_msgs/Float32"
+  })
+
+  topic.publish(message);
+});
+
+const botaoPrintar_a_p = document.getElementById("botao-angular-p");
+botaoPrintar_a_p.addEventListener("click", function() {
+  const inputValor = document.getElementById("input-valor_ap");
+  const valor = inputValor.value;
+  console.log(valor);
+
+  
+  var message = new ROSLIB.Message({
+    data: parseFloat(valor)
+  })
+
+  var topic = new ROSLIB.Topic({
+    ros: ros,
+    name: "/control/position/setup/angular/kp",
+    messageType: "std_msgs/Float32"
+  })
+
+  topic.publish(message);
+});
+
+const botaoPrintar_a_i = document.getElementById("botao-angular-i");
+botaoPrintar_a_i.addEventListener("click", function() {
+  const inputValor = document.getElementById("input-valor_ai");
+  const valor = inputValor.value;
+  console.log(valor);
+
+  
+  var message = new ROSLIB.Message({
+    data: parseFloat(valor)
+  })
+
+  var topic = new ROSLIB.Topic({
+    ros: ros,
+    name: "/control/position/setup/angular/ki",
+    messageType: "std_msgs/Float32"
+  })
+
+  topic.publish(message);
+});
+
+const botaoPrintar_a_d = document.getElementById("botao-angular-d");
+botaoPrintar_a_d.addEventListener("click", function() {
+  const inputValor = document.getElementById("input-valor_ad");
+  const valor = inputValor.value;
+  console.log(valor);
+
+  
+  var message = new ROSLIB.Message({
+    data: parseFloat(valor)
+  })
+
+  var topic = new ROSLIB.Topic({
+    ros: ros,
+    name: "/control/position/setup/angular/kd",
+    messageType: "std_msgs/Float32"
+  })
+
+  topic.publish(message);
+});
